2ea0072a9142656196529fa9daf5aef60a6a1850
Samady Correa <samadybiasoli@gmail.com>
2025-06-14 17:29:01 -0300

    RTC code was integrated to the code
    Set up libraries so far: WebServer, FreeRTOS,  Firebase, RTClib

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,20 @@
 #include <WebServer.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <FirebaseESP32.h>
+#include <RTClib.h>
 
 // Replace with your network credentials
 const char* ssid = "iPhone de Samady"; // Mantenha suas credenciais
 const char* password = "sepamadyn";    // Mantenha suas credenciais
+
+// Defining the API Key and the Firebase RTDB URL
+const char* firebase_api_key =  "";
+const char* database_url = "";
+
+// Define the user Email and password that is used to authenticate with Firebase
+const char* user_email = "";
+const char* user_password = "";
 
 // Assign output variables to GPIO pins
 const int ledPin = 25;
@@ -19,13 +29,31 @@ String output27State = "off"; // Estado inicial do GPIO 27
 // Create a web server object
 WebServer server(80);
 
+// Create an RTC object
+RTC_DS3231 rtc;
+const int CLOCK_INTERRUPT_PIN = 2;
+volatile bool interruptFlag = false;
+
+char diasDaSemana[7][12] = {"Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado"};
+
+// Define Firebase Data object
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
 // Creating FreeRTOS task handles
 TaskHandle_t mainTaskHandle;   // Renomeado para clareza (Handle no final)
 TaskHandle_t serverTaskHandle; // Renomeado para clareza
+TaskHandle_t RTCTaskHandle;
+TaskHandle_t FirebaseTaskHandle;
 
 // Function prototypes for tasks
 void MainTask(void *pvParameters);
 void ServerTask(void *pvParameters);
+void RTCTask(void *pvParameters);
+void FirebaseTask(void *pvParameters);
 
 // Function to handle the root URL and show the current states
 void handleRoot() {
@@ -88,6 +116,51 @@ void handleGPIO27Off() {
   handleRoot();
 }
 
+void tratarInterrupcao() {
+  interruptFlag = true;
+}
+
+void imprimirDataHora(DateTime momento) {
+  Serial.print("Data: ");
+  Serial.print(momento.day(), DEC);
+  Serial.print('/');
+  Serial.print(momento.month(), DEC);
+  Serial.print('/');
+  Serial.print(momento.year(), DEC);
+  Serial.print(" / Dia da semana: ");
+  Serial.print(diasDaSemana[momento.dayOfTheWeek()]);
+  Serial.print(" / Hora: ");
+  if (momento.hour() < 10) Serial.print("0");
+  Serial.print(momento.hour(), DEC);
+  Serial.print(':');
+  if (momento.minute() < 10) Serial.print("0");
+  Serial.print(momento.minute(), DEC);
+  Serial.print(':');
+  if (momento.second() < 10) Serial.print("0");
+  Serial.print(momento.second(), DEC);
+  Serial.print(" / Temperatura: ");
+  Serial.print(rtc.getTemperature());
+  Serial.println(" °C");
+  return;
+}
+
+void agendarProximoCiclo() {
+  DateTime agora = rtc.now();
+  
+  // Próximo Alarme 1 (LIGA) em 30 segundos a partir de agora
+  DateTime proximoAlarme1 = agora + TimeSpan(0, 0, 0, 30);
+  
+  // Configura os alarmes
+  rtc.setAlarm1(proximoAlarme1, DS3231_A1_Date);
+  
+  Serial.println("\n--- Próximo ciclo agendado ---");
+  Serial.print("Próximo Alarme 1 (LIGA): ");
+  imprimirDataHora(proximoAlarme1);
+  Serial.print("Próximo Alarme 2 (DESLIGA): ");
+
+  return;
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Iniciando Setup...");
@@ -95,11 +168,12 @@ void setup() {
   // Initialize the output variables as outputs
   pinMode(output26, OUTPUT);
   pinMode(output27, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   // Set outputs to LOW initially
   digitalWrite(output26, LOW);
   digitalWrite(output27, LOW);
 
-  // Connect to Wi-Fi network
+  // Connect to Wi-Fi network --------------------------------------------------------------------------------------
   Serial.print("Conectando a ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -131,7 +205,7 @@ void setup() {
   server.begin();
   Serial.println("Servidor HTTP iniciado");
 
-  // Criando as tasks do FreeRTOS
+  // Criando as tasks do FreeRTOS --------------------------------------------------------------------------------------
   // Para ESP32, o Core 0 (PRO_CPU) é frequentemente usado para Wi-Fi e tarefas de sistema.
   // O Core 1 (APP_CPU) é geralmente para código de aplicação.
   // É uma boa prática executar tarefas de aplicação como o servidor web no Core 1.
@@ -156,10 +230,90 @@ void setup() {
     &serverTaskHandle,// Handle da Task
     1                 // Core onde a Task será executada (1 para APP_CPU)
   );
+
+  xTaskCreatePinnedToCore(
+    RTCTask,       // Função da Task
+    "RTCTask",     // Nome da Task
+    4096,             // Tamanho da Stack (Aumentar para 8192 se houver problemas de stack com WebServer)
+    NULL,             // Parâmetros da Task
+    1,                // Prioridade da Task (maior que MainTask para responsividade do servidor)
+    &RTCTaskHandle,// Handle da Task
+    1                 // Core onde a Task será executada (1 para APP_CPU)
+  );
+
+  xTaskCreatePinnedToCore(
+    FirebaseTask,       // Função da Task
+    "FirebaseTask",     // Nome da Task
+    4096,             // Tamanho da Stack (Aumentar para 8192 se houver problemas de stack com WebServer)
+    NULL,             // Parâmetros da Task
+    1,                // Prioridade da Task (maior que MainTask para responsividade do servidor)
+    &FirebaseTaskHandle,// Handle da Task
+    1                 // Core onde a Task será executada (1 para APP_CPU)
+  );
   
   Serial.println("Tasks criadas. Setup concluído.");
   // O scheduler do FreeRTOS já está rodando neste ponto no ESP32 Arduino Core.
   // setup() será finalizada, e as tasks começarão a executar.
+
+  // Firebase Setup --------------------------------------------------------------------------------------
+  // Assign the api key (required) 
+  config.api_key = firebase_api_key;
+
+  // Assign the user sign in credentials
+  auth.user.email = user_email;
+  auth.user.password = user_password;
+
+  // Assign the RTDB URL (required) 
+  config.database_url = database_url;
+  Firebase.reconnectNetwork(true);
+
+  // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
+  Firebase.begin(&config, &auth);
+  Firebase.setDoubleDigits(5);
+  config.timeout.serverResponse = 10 * 1000;
+
+  // Inicializa o RTC  --------------------------------------------------------------------------------------
+  if (!rtc.begin()) {
+    Serial.println("RTC não encontrado!");
+    while (1);
+  }
+
+  // Se perdeu energia, ajusta a hora atual para a hora da compilação
+  if (rtc.lostPower()) {
+    Serial.println("RTC perdeu a hora. Ajustando...");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  // Desativa saída 32kHz e alarmes antigos
+  rtc.disable32K();
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+  rtc.writeSqwPinMode(DS3231_OFF); // Garante que INT/SQW está no modo interrupção
+
+  // Configura pino da interrupção e função de tratamento
+  pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), tratarInterrupcao, FALLING);
+
+  // Configura os alarmes iniciais
+  DateTime agora = rtc.now();
+  
+  // Alarme 1 (LIGA) - Ativa em 1 minuto
+  DateTime alarme1 = agora + TimeSpan(0, 0, 0, 30); // +30 segundos
+
+  // Configura os alarmes no RTC
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  rtc.setAlarm1(alarme1, DS3231_A1_Date);
+
+  Serial.println("Sistema iniciado!");
+  Serial.println("Modo de operação: Alarme 1 LIGA, Alarme 2 DESLIGA");
+  Serial.print("Alarme 1 (LIGA) agendado para: ");
+  imprimirDataHora(alarme1);
+
 }
 
 void loop() {
@@ -172,10 +326,6 @@ void loop() {
 // Implementação da MainTask
 void MainTask(void *pvParameters) {
   Serial.println("MainTask: Iniciada no Core " + String(xPortGetCoreID()));
-  
-  // Configura o pino do LED como saída DENTRO da task.
-  // Isso é feito uma vez quando a task inicia.
-  pinMode(ledPin, OUTPUT);
   Serial.println("MainTask: GPIO " + String(ledPin) + " configurado como OUTPUT para piscar LED.");
 
   bool ledState = LOW;
@@ -189,7 +339,6 @@ void MainTask(void *pvParameters) {
     } else {
       Serial.println("MainTask: LED OFF"); // Descomente para log detalhado
     }
-    
     vTaskDelay(pdMS_TO_TICKS(500)); // Pisca a cada 500ms (meio segundo ON, meio segundo OFF)
   }
 }
@@ -203,5 +352,43 @@ void ServerTask(void *pvParameters) {
     // Pequeno delay para não sobrecarregar o processador e permitir que outras tasks executem.
     // 10ms é um valor comum. Se a resposta do servidor parecer lenta, pode reduzir para 1ms.
     vTaskDelay(pdMS_TO_TICKS(10)); 
+  }
+}
+
+
+void RTCTask(void *pvParameters) {
+  Serial.println("RTCTask: Iniciada no Core " + String(xPortGetCoreID()));
+  while (true) {
+      if (rtc.alarmFired(1)) {
+        // Se o alarme 1 disparou, execute a ação desejada
+        rtc.clearAlarm(1);
+        digitalWrite(output26, !digitalRead(output26));
+        Serial.println("\n>>> ALARME 1 DISPARADO - LED LIGADO <<<");
+        Serial.print("Hora do disparo: ");
+        imprimirDataHora(rtc.now());
+        agendarProximoCiclo();
+    }
+    
+    // Pequeno delay para não sobrecarregar o processador e permitir que outras tasks executem.
+    // 10ms é um valor comum. Se a resposta do servidor parecer lenta, pode reduzir para 1ms.
+    vTaskDelay(pdMS_TO_TICKS(10)); 
+  }
+}
+
+void FirebaseTask(void *pvParameters) {
+  Serial.println("FirebaseTask: Iniciada no Core " + String(xPortGetCoreID()));
+  int ledState;
+  while (true) {
+    Serial.println("Firebase ESP32 Client v4.4.0");
+    if (Firebase.ready() && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0)){
+      sendDataPrevMillis = millis();
+      if(Firebase.RTDB.getInt(&fbdo, "/led1/state", &ledState)){
+        digitalWrite(ledPin, ledState);
+      }else{
+        Serial.println(fbdo.errorReason().c_str());
+      }
+    }
+    // Pequeno delay para não sobrecarregar o processador e permitir que outras tasks executem.
+    vTaskDelay(pdMS_TO_TICKS(1000)); // Verifica a cada segundo
   }
 }
